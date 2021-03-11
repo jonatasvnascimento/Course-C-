c74a1ccfbf97c55c5363b4222641b892e74071f0
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-03-11 11:11:06 -0300

    Menu

--- Projects/Menus/Form1.Designer.cs
+++ Projects/Menus/Form1.Designer.cs
@@ -29,13 +29,191 @@ namespace Menus
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menu_geral = new System.Windows.Forms.MenuStrip();
+            this.menu_file = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_new = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_open = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_new_project = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_new_file = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.menu_exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.propetiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu_combo_1 = new System.Windows.Forms.ToolStripComboBox();
+            this.label_resultado = new System.Windows.Forms.Label();
+            this.menu_geral.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(713, 408);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menu_geral
+            // 
+            this.menu_geral.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menu_geral.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menu_geral.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu_file,
+            this.editToolStripMenuItem,
+            this.menu_combo_1});
+            this.menu_geral.Location = new System.Drawing.Point(0, 0);
+            this.menu_geral.Name = "menu_geral";
+            this.menu_geral.Size = new System.Drawing.Size(800, 37);
+            this.menu_geral.TabIndex = 1;
+            this.menu_geral.Text = "menuStrip1";
+            // 
+            // menu_file
+            // 
+            this.menu_file.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu_new,
+            this.menu_open,
+            this.toolStripSeparator1,
+            this.menu_exit});
+            this.menu_file.Name = "menu_file";
+            this.menu_file.Size = new System.Drawing.Size(54, 33);
+            this.menu_file.Text = "File";
+            // 
+            // menu_new
+            // 
+            this.menu_new.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu_new_project,
+            this.menu_new_file});
+            this.menu_new.Name = "menu_new";
+            this.menu_new.Size = new System.Drawing.Size(270, 34);
+            this.menu_new.Text = "New";
+            // 
+            // menu_open
+            // 
+            this.menu_open.Name = "menu_open";
+            this.menu_open.Size = new System.Drawing.Size(270, 34);
+            this.menu_open.Text = "Open";
+            this.menu_open.Click += new System.EventHandler(this.menu_open_Click);
+            // 
+            // menu_new_project
+            // 
+            this.menu_new_project.Name = "menu_new_project";
+            this.menu_new_project.Size = new System.Drawing.Size(270, 34);
+            this.menu_new_project.Text = "Project";
+            // 
+            // menu_new_file
+            // 
+            this.menu_new_file.Name = "menu_new_file";
+            this.menu_new_file.Size = new System.Drawing.Size(270, 34);
+            this.menu_new_file.Text = "File";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(267, 6);
+            // 
+            // menu_exit
+            // 
+            this.menu_exit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.menu_exit.Name = "menu_exit";
+            this.menu_exit.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.menu_exit.Size = new System.Drawing.Size(270, 34);
+            this.menu_exit.Text = "Exit";
+            this.menu_exit.Click += new System.EventHandler(this.menu_exit_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.pasteToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.propetiToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(58, 33);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.copyToolStripMenuItem.Text = "Copy";
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.pasteToolStripMenuItem.Text = "Paste";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(267, 6);
+            // 
+            // propetiToolStripMenuItem
+            // 
+            this.propetiToolStripMenuItem.Name = "propetiToolStripMenuItem";
+            this.propetiToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.propetiToolStripMenuItem.Text = "Properties...";
+            // 
+            // menu_combo_1
+            // 
+            this.menu_combo_1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.menu_combo_1.Name = "menu_combo_1";
+            this.menu_combo_1.Size = new System.Drawing.Size(200, 33);
+            this.menu_combo_1.SelectedIndexChanged += new System.EventHandler(this.menu_combo_1_SelectedIndexChanged);
+            this.menu_combo_1.Click += new System.EventHandler(this.menu_combo_1_Click);
+            // 
+            // label_resultado
+            // 
+            this.label_resultado.AutoSize = true;
+            this.label_resultado.Location = new System.Drawing.Point(13, 98);
+            this.label_resultado.Name = "label_resultado";
+            this.label_resultado.Size = new System.Drawing.Size(51, 20);
+            this.label_resultado.TabIndex = 2;
+            this.label_resultado.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_resultado);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menu_geral);
+            this.MainMenuStrip = this.menu_geral;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menu_geral.ResumeLayout(false);
+            this.menu_geral.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menu_geral;
+        private System.Windows.Forms.ToolStripMenuItem menu_file;
+        private System.Windows.Forms.ToolStripMenuItem menu_new;
+        private System.Windows.Forms.ToolStripMenuItem menu_new_project;
+        private System.Windows.Forms.ToolStripMenuItem menu_new_file;
+        private System.Windows.Forms.ToolStripMenuItem menu_open;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem menu_exit;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem propetiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox menu_combo_1;
+        private System.Windows.Forms.Label label_resultado;
     }
 }
 
--- Projects/Menus/Form1.cs
+++ Projects/Menus/Form1.cs
@@ -15,5 +15,45 @@ namespace Menus
         {
             InitializeComponent();
         }
+
+        private void menu_exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menu_open_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Open");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*Add opções so menu_combo1*/
+            menu_combo_1.Items.Add("Valor 1");
+            menu_combo_1.Items.Add("Valor 2");
+            menu_combo_1.Items.Add("Valor 3");
+            menu_combo_1.Items.Add("Valor 4");
+            menu_combo_1.Items.Add("Valor 5");
+
+
+        }
+
+        private void menu_combo_1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void menu_combo_1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (menu_combo_1.Text == "Valor 1")
+                MessageBox.Show("Valor 1");
+
+            label_resultado.Text = menu_combo_1.Text;
+        }
     }
 }
