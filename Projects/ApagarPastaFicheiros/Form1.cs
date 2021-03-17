f25295cae34c8dddce3889a81f954608686658c3
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-03-17 10:25:24 -0300

    Buscar apagar deletar arquivos

--- Projects/ApagarPastaFicheiros/Form1.cs
+++ Projects/ApagarPastaFicheiros/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace ApagarPastaFicheiros
 {
@@ -14,6 +15,42 @@ namespace ApagarPastaFicheiros
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] lista_ficheiros = Directory.GetFiles(@"C:\", "*.dat");
+
+            foreach (string item in lista_ficheiros)
+            {
+                File.Move(item, @"C:\config\" + Path.GetFileName(item));
+            }
+            MessageBox.Show("Terminado");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //File.Delete(@"C:\dados.dat");
+            if(Directory.Exists(@"C:\config"))
+                Directory.Delete(@"C:\config", true);
+
+            MessageBox.Show("Terminado");
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //* todos arquivos 
+            //s*.* todos os arquivos com começar com s
+            //*t*.* tudo que tem t no meio vai trazer
+            //*.
+            
+            string[] ficheiros = Directory.GetFiles(@"C:\dados");
+
+            foreach (string item in ficheiros)
+                listBox1.Items.Add(Path.GetFileNameWithoutExtension(item));
+
+            //listBox1.Items.AddRange(ficheiros); busca ficheiro
         }
     }
 }
