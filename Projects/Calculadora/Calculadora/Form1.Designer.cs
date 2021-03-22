955dcf3b0a028e6583c65274e5d949a53d732cb6
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-03-22 11:23:52 -0300

    Calculadora 1.0 signal

---
+++ Projects/Calculadora/Calculadora/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmd_sair = new System.Windows.Forms.Button();
+            this.cmd_calcular = new System.Windows.Forms.Button();
+            this.text_operacao = new System.Windows.Forms.TextBox();
+            this.label_algoritmo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmd_sair
+            // 
+            this.cmd_sair.Location = new System.Drawing.Point(55, 124);
+            this.cmd_sair.Name = "cmd_sair";
+            this.cmd_sair.Size = new System.Drawing.Size(112, 34);
+            this.cmd_sair.TabIndex = 2;
+            this.cmd_sair.Text = "Sair";
+            this.cmd_sair.UseVisualStyleBackColor = true;
+            this.cmd_sair.Click += new System.EventHandler(this.cmd_sair_Click);
+            // 
+            // cmd_calcular
+            // 
+            this.cmd_calcular.Location = new System.Drawing.Point(173, 124);
+            this.cmd_calcular.Name = "cmd_calcular";
+            this.cmd_calcular.Size = new System.Drawing.Size(112, 34);
+            this.cmd_calcular.TabIndex = 1;
+            this.cmd_calcular.Text = "Calcular";
+            this.cmd_calcular.UseVisualStyleBackColor = true;
+            this.cmd_calcular.Click += new System.EventHandler(this.cmd_calcular_Click);
+            // 
+            // text_operacao
+            // 
+            this.text_operacao.Location = new System.Drawing.Point(55, 87);
+            this.text_operacao.MaxLength = 30;
+            this.text_operacao.Name = "text_operacao";
+            this.text_operacao.Size = new System.Drawing.Size(230, 31);
+            this.text_operacao.TabIndex = 0;
+            // 
+            // label_algoritmo
+            // 
+            this.label_algoritmo.AutoSize = true;
+            this.label_algoritmo.Location = new System.Drawing.Point(55, 59);
+            this.label_algoritmo.Name = "label_algoritmo";
+            this.label_algoritmo.Size = new System.Drawing.Size(177, 25);
+            this.label_algoritmo.TabIndex = 3;
+            this.label_algoritmo.Text = "Escreva o algoritimo:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 205);
+            this.Controls.Add(this.label_algoritmo);
+            this.Controls.Add(this.text_operacao);
+            this.Controls.Add(this.cmd_calcular);
+            this.Controls.Add(this.cmd_sair);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora 1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmd_sair;
+        private System.Windows.Forms.Button s;
+        private System.Windows.Forms.TextBox text_operacao;
+        private System.Windows.Forms.Label label_algoritmo;
+        private System.Windows.Forms.Button cmd_calcular;
+    }
+}
+
---
+++ Projects/Calculadora/Calculadora/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmd_sair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cmd_calcular_Click(object sender, EventArgs e)
+        {
+            int parcela1;
+            int parcela2;
+            string tipo_operação;
+            string algoritmo = text_operacao.Text;
+            string sinais = "+-*/";
+            int index = 0;
+
+            //verificação do que tem no campo text_operação
+            foreach (char c in algoritmo)
+            {
+                foreach (char s in sinais)
+                {
+                    if (c == s)
+                    {
+                        //define o calculo a ser feito com o operador achado do laço
+                        parcela1 = int.Parse(algoritmo.Substring(0, index));
+                        //sinal
+                        tipo_operação = c.ToString();
+                        //parcela2
+                        parcela2 = int.Parse(algoritmo.Substring(index + 1));
+                    }
+                }
+                index++;
+            }
+        }
+    }
+}
