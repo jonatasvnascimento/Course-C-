f75ac72d8718fcee75f2f380bb782983627ec2ec
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-09-28 16:49:15 -0300

    02 aula 04 artigo

--- Csharp_Alura/Aulas/certificacao-csharp-pt1/certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/3 - Tipos de Ponto Flutuante/depois/PontoFlutuante.cs
+++ Csharp_Alura/Aulas/certificacao-csharp-pt1/certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/3 - Tipos de Ponto Flutuante/depois/PontoFlutuante.cs
@@ -10,6 +10,32 @@ namespace certificacao_csharp_roteiro.antes
     {
         public void Executar()
         {
+            float idade = 15;
+            idade = 15.5f;
+
+            //int massa = 6_000000_000000_000000;
+            //long massa = 6e24;
+            Console.WriteLine($"long.MinValue: {long.MinValue}");
+            Console.WriteLine($"long.MaxValue: {long.MaxValue}");
+
+            float massaDaTerra = 5.9736e24f; //System.Single
+            Console.WriteLine($"massaDaTerra: {massaDaTerra}");
+
+            float numeroPI = 3.14159f; // System.Single
+            Console.WriteLine($"numeroPI: {numeroPI}");
+
+            //float numeroMuitoMaior = 6e100f
+            double numeroMuitoMaior = 6e100d;
+
+            Console.WriteLine();
+            Console.WriteLine("Operação com int, float e short");
+
+            int x = 3;
+            short y = 5;
+
+            var resultado1 = x * y;
+            Console.WriteLine("x * y= (0)", resultado1);
+            Console.WriteLine($"O resultado é do tipo:  {resultado1.GetType()}");
         }
     }
 }
--- Csharp_Alura/Aulas/certificacao-csharp-pt1/certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/4 - Decimal/depois/Decimal.cs
+++ Csharp_Alura/Aulas/certificacao-csharp-pt1/certificacao-csharp-pt1/Aula1 - criar tipos de valor, incluindo estruturas e enum/4 - Decimal/depois/Decimal.cs
@@ -10,6 +10,33 @@ namespace certificacao_csharp_roteiro
     {
         public void Executar()
         {
+            decimal materiaPrima = 10.1m;
+            decimal maoDeObra = 20.2m;
+            decimal custo = 30.3m;
+
+            Console.WriteLine("Descobrindo se (10.1m + 20.2m) == 30.3m");
+            Console.WriteLine((materiaPrima + maoDeObra) == custo);
+
+            float valorProduto1 = 10.10f;
+            float valorProduto2 = 20.20f;
+            float subtotal = 30.30f;
+
+            Console.WriteLine("Descobrindo se (10.10 + 20.20) == 30.30");
+            Console.WriteLine((valorProduto1 + valorProduto2) == subtotal);
+
+            Console.WriteLine(valorProduto1 + valorProduto2);
+
+            Console.WriteLine("Descobrindo se (10.1 + 20.2) == 30.299999999999997");
+            Console.WriteLine((valorProduto1 + valorProduto2) == 30.299999999999997);
+
+            decimal valorProduto10 = 10.10m;
+            decimal valorProduto20 = 20.20m;
+            decimal subtotal0 = 30.30m;
+
+            Console.WriteLine("Descobrindo se (10.10 + 20.20) == 30.30");
+            Console.WriteLine((valorProduto10 + valorProduto20) == subtotal0);
+
+            Console.WriteLine(valorProduto1 + valorProduto2);
         }
     }
 }
