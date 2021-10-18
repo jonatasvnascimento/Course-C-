7a1a0f4e54b19ea3024e7f1a356191c58d91d8f2
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-10-18 20:25:56 -0300

    3

---
+++ Csharp_Alura/Aulas/aula3/certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/1 - Tipos de Referência/depois/TiposDeReferencia.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class TiposDeReferencia : IAulaItem
+    {
+        public void Executar()
+        {
+            int idade = 42;
+            int copiaIdade = idade;
+
+            Console.WriteLine("int idade = 42;");
+            Console.WriteLine($"idade: { idade}");
+            Console.WriteLine($"copiaIdade: {copiaIdade}");
+        }
+
+        class Cliente
+        {
+            public Cliente()
+            {
+            }
+
+
+
+            public string Nome { get; set; }
+            public int Idade { get; set; }
+
+            public override string ToString()
+            {
+                return $"Nome: {Nome} Idade: {Idade}";
+            }
+
+        }
+
+    }
+}
---
+++ Csharp_Alura/Aulas/aula3/certificacao-csharp-pt1/Aula2 - criar tipos de referência, tipos genéricos, construtores, variáveis ​​estáticas/2 - Classes/depois/Classes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class Classes : IAulaItem
+    {
+        public void Executar()
+        {
+            Cliente teste = new Cliente()
+            {
+                Nome = "Teste",
+                Idade = 99
+            };
+            Console.WriteLine(teste);
+
+        }
+
+        class Cliente
+        {
+            public Cliente()
+            {
+            }
+
+
+
+            public string Nome { get; set; }
+            public int Idade { get; set; }
+
+            public override string ToString()
+            {
+                return $"Nome: {Nome} Idade: {Idade}";
+            }
+
+        }
+
+    }
+}
